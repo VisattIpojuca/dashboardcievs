9836b40e874b7246a06809fd49e49ff4a87e8c92
VisattIpojuca <visattipojuca@gmail.com>
2025-11-26 20:24:03 -0300

    Update Home.py

--- Home.py
+++ Home.py
@@ -33,7 +33,7 @@ html, body, [data-testid="stAppViewContainer"], * {
 
 /* Títulos (cor fixa) */
 h1, h2, h3, h4, h5, h6 {
-    color: var(--azul-principal) !important;
+    color: var(--azul-secundario) !important;
 }
 
 /* Parágrafos */
